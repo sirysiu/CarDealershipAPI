1052aba73978e69c3192230a0c80f3c5a7a66489
sirysiu <sirisew@gmail.com>
2024-12-13 10:21:51 -0500

    the config so it would get from the properties instead of hard coded in

--- dealership-api/src/main/java/com/pluralsight/dealership/config/AppConfig.java
+++ dealership-api/src/main/java/com/pluralsight/dealership/config/AppConfig.java
@@ -1,18 +1,29 @@
 package com.pluralsight.dealership.config;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
 
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
     @Bean
     public BasicDataSource dataSource() {
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setUrl("jdbc:mysql://localhost:3306/your_db");  // Replace with your DB URL
-        dataSource.setUsername("yourUsername");  // Replace with your DB username
-        dataSource.setPassword("yourPassword");  // Replace with your DB password
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
         return dataSource;
     }
 }
